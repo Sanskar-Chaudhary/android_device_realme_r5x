7893233e3e019a74c9bb16c8c230aebc91dbe9aa
Amrito Karmokar <amritokarmokar5i@gmail.com>
2022-09-25 23:18:30 +0600

    r5x: Use clang-r416183b1

--- BoardConfig.mk
+++ BoardConfig.mk
@@ -81,6 +81,8 @@ BOARD_RAMDISK_OFFSET := 0x01000000
 BOARD_KERNEL_SEPARATED_DTBO := true
 TARGET_KERNEL_SOURCE := kernel/realme/r5x
 TARGET_KERNEL_CLANG_COMPILE := true
+TARGET_KERNEL_CLANG_VERSION := clang-r416183b1
+TARGET_KERNEL_CLANG_PATH := $(shell pwd)/prebuilts/clang/host/linux-x86/clang-r416183b1
 TARGET_KERNEL_CONFIG := vendor/RMX1911_defconfig
 TARGET_KERNEL_ADDITIONAL_FLAGS := \
    HOSTCFLAGS="-fuse-ld=lld -Wno-unused-command-line-argument"
